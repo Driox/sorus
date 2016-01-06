bc6c7c84fa3f0964906b9843d2a67dea6f659df0
Adrien <adrien.crovetto@gmail.com>
2016-01-06 15:26:24 +0100

    add Sorus test

--- test/scala/helpers/SorusTest.scala
+++ test/scala/helpers/SorusTest.scala
@@ -1,12 +1,104 @@
 package helpers
 
+import helpers.SorusDSL._
+
 import org.scalatest._
+import play.api.libs.json.{JsError, JsSuccess}
 
-class SorusTest extends FlatSpec with Matchers {
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.util.{Try, Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.postfixOps
 
-  "Sorus" should "work from within a browser" in {
-      val x = 1 + 1
-      x should be(2)
+import scalaz.syntax.either._
+import scalaz._
+
+class SorusTest extends FlatSpec with Matchers with Sorus {
+
+  // Just the fact this function compile allow us to compose various Future
+  def composeVariousFuture():Future[Fail\/String] = {
+
+    for{
+      foo <- Future.successful("foo")      ?| "future.error"
+      bar <- Future.successful(Some(foo))  ?| "future.option.error"
+      baz <- Future.successful(Right(bar)) ?| "future.either.error"
+      foz <- Future.successful(baz.right)  ?| "future.disjunction.error"
+      faz <- Some(foz)                     ?| "option.error"
+      far <- Right(faz)                    ?| "either.error"
+      boo <- far.right                     ?| "disjunction.error"
+      boz <- Try { boo }                   ?| "try.error"
+      bor <- (boz == "foo")                ?| "boolean.error"
+    } yield {
+      boz
     }
-  
+  }
+
+    "Sorus" should "properly promote Future[A] to Step[A]" in {
+      val successfulFuture = Future.successful(42)
+      Await.result((successfulFuture ?| "error").run, 10 seconds) shouldBe 42.right
+
+      val ex = new NullPointerException
+      val failedFuture = Future.failed[Int](ex)
+      Await.result((failedFuture ?| "error").run, 10 seconds) shouldBe Fail("error", Some(-\/(ex))).left
+    }
+
+    "Sorus" should "properly promote Future[Option[A]] to Step[A]" in {
+      val someFuture = Future.successful(Some(42))
+      Await.result((someFuture ?| "error").run, 10 seconds) shouldBe 42.right
+
+      val noneFuture = Future.successful[Option[Int]](None)
+      Await.result((noneFuture ?| "error").run, 10 seconds) shouldBe Fail("error", Some(\/-(Fail("()",None)))).left
+    }
+
+    "Sorus" should "properly promote Future[Either[B, A]] to Step[A]" in {
+      val rightFuture = Future.successful[Either[String, Int]](Right(42))
+      Await.result((rightFuture ?| "error").run, 10 seconds) shouldBe 42.right
+
+      val leftFuture = Future.successful[Either[String, Int]](Left("foo"))
+      val eitherT = leftFuture ?| "error"
+      Await.result(eitherT.run, 10 seconds) shouldBe Fail("error", Some(\/-(Fail("foo",None)))).left
+    }
+
+    "Sorus" should "properly promote Future[B \\/ A] to Step[A]" in {
+      val rightFuture = Future.successful(42.right[String])
+      Await.result((rightFuture ?| "error").run, 10 seconds) shouldBe 42.right
+
+      val leftFuture = Future.successful(-\/("foo"))
+      val eitherT = leftFuture ?| "error"
+      Await.result(eitherT.run, 10 seconds) shouldBe Fail("error", Some(\/-(Fail("foo",None)))).left
+    }
+
+    "Sorus" should "properly promote Option[A] to Step[A]" in {
+      val some = Some(42)
+      Await.result((some ?| "error").run, 10 seconds) shouldBe 42.right
+      
+      val none = None
+      Await.result((none ?| "error").run, 10 seconds) shouldBe Fail("error", Some(\/-(Fail("()",None)))).left
+    }
+
+    "Sorus" should "properly promote Either[A, B] to Step[A]" in {
+      val right = Right(42)
+      Await.result((right ?| "error").run, 10 seconds) shouldBe 42.right
+      
+      val left = Left("foo")
+      val eitherT = left ?| "error"
+      Await.result(eitherT.run, 10 seconds) shouldBe Fail("error", Some(\/-(Fail("foo",None)))).left
+    }
+
+    "Sorus" should "properly promote Boolean to Step[A]" in {
+      Await.result((true ?| "error").run, 10 seconds) shouldBe ().right
+      Await.result((false ?| "error").run, 10 seconds) shouldBe Fail("error",Some(\/-(Fail("()",None)))).left
+    }
+
+    "Sorus" should "properly promote Try[A] to Step[A]" in {
+      val success = Success(42)
+      Await.result((success ?| "error").run, 10 seconds) shouldBe 42.right
+
+      val ex = new Exception("foo")
+      val failure = Failure(ex)
+      val eitherT = failure ?| "error"
+      Await.result(eitherT.run, 10 seconds) shouldBe Fail("error",Some(-\/(ex))).left
+    }
+
 }
